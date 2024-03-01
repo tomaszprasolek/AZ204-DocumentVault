8a40e03de289f80c453995d9ab3275775fe509c8
Tomasz Prasołek <tomasz.prasolek@gmail.com>
2024-03-01 15:43:01 +0100

    Connect function with blob storage

--- AZ204-DocumentVault/AZ204-AzureFunctions/HttpTriggerGenerateDownloadLink.cs
+++ AZ204-DocumentVault/AZ204-AzureFunctions/HttpTriggerGenerateDownloadLink.cs
@@ -1,4 +1,7 @@
-﻿using Azure.Storage.Blobs;
+﻿using AZ204_AzureFunctions.Models;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Blobs;
 using Azure.Storage.Sas;
 using System;
 using Microsoft.AspNetCore.Mvc;
@@ -6,7 +9,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -14,10 +16,9 @@ namespace AZ204_AzureFunctions;
 
 public static class HttpTriggerGenerateDownloadLink
 {
-    [FunctionName("HttpGenerateDownloadLink")]
+    [FunctionName("GenerateDownloadLink")]
     public static async Task<IActionResult> RunAsync(
         [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-        //[FromBody] BlobInfo blobInfo,
         ILogger log)
     {
         log.LogInformation("C# HTTP trigger function processed a request.");
@@ -25,17 +26,14 @@ public static class HttpTriggerGenerateDownloadLink
         BlobInfo blobInfo = await JsonSerializer.DeserializeAsync<BlobInfo>(req.Body, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         log.LogInformation(blobInfo.ToString());
         
-        // string link = GenerateLink(blobInfo);
-        string link = "test001";
+        string link = await GenerateLink(blobInfo);
 
         return new OkObjectResult(link);
     }
 
-    private static string GenerateLink(BlobInfo blobInfo)
+    private static async Task<string> GenerateLink(BlobInfo blobInfo)
     {
-        return "test-link";
-        
-        BlobContainerClient containerClient = GetBlobContainerClient();
+        BlobContainerClient containerClient = await GetBlobContainerClientAsync();
 
         BlobClient blobClient = containerClient.GetBlobClient(blobInfo.FileName);
 
@@ -44,9 +42,9 @@ public static class HttpTriggerGenerateDownloadLink
         return result.ToString();
     }
 
-    private static BlobContainerClient GetBlobContainerClient()
+    private static async Task<BlobContainerClient> GetBlobContainerClientAsync()
     {
-        string storageAccountKey = Environment.GetEnvironmentVariable("StorageAccountKey");
+        string storageAccountKey = await GetSecretAsync("StorageAccountKey");
         string storageAccountName = Environment.GetEnvironmentVariable("StorageAccountName");
         string containerName = Environment.GetEnvironmentVariable("ContainerName");
         
@@ -57,15 +55,13 @@ public static class HttpTriggerGenerateDownloadLink
         BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
         return containerClient;
     }
-}
 
-public class BlobInfo
-{
-    public string FileName { get; set; }
-    public int HoursToBeExpired { get; set; }
-
-    public override string ToString()
+    private static async Task<string> GetSecretAsync(string secretName)
     {
-        return $"File name: {FileName}, time left (in hours) when link expired: {HoursToBeExpired}";
+        // string keyVaultUri = Environment.GetEnvironmentVariable("KeyVaultUri")!;
+        string keyVaultUri = "https://documentskeyvault.vault.azure.net/";
+        var secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+        Azure.Response<KeyVaultSecret> secretResponse = await secretClient.GetSecretAsync(secretName);
+        return secretResponse.Value.Value;
     }
 }
